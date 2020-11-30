73e539ab355251bad79aae4af8d70cc2e8bf5360
Miguel Rodriguez Alvarez <miguel7.254@gmail.com>
2020-11-30 20:36:08 +0100

    valoraciones

---
+++ Practica 2/javascript/rateyo.js
@@ -0,0 +1,41 @@
+$(function () {
+
+    $(".rateyo").rateYo(
+        {
+            rating: 4
+        }
+    );
+    $(".rateyo1").rateYo(
+        {
+            rating: 4,
+            starWidth: "20px"
+        }
+    );
+    $(".rateyo2").rateYo(
+        {
+            rating: 4,
+            starWidth: "20px"
+        }
+    );
+    $(".rateyo3").rateYo(
+        {
+            rating: 1,
+            starWidth: "20px"
+        }
+    );
+    $(".rateyo4").rateYo(
+        {
+            rating: 5,
+            starWidth: "20px"
+        }
+    );
+});
+
+function changeText() {
+    var boton = document.getElementById(("btnValoracion"));
+    if (boton.value === "Ver valoraciones") {
+        boton.value = "Ocultar valoraciones";
+    } else {
+        boton.value = "Ver valoraciones";
+    }
+}
